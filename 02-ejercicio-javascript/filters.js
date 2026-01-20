726615f563336c6ab9233f885583fb4fcee3d12f
madeval <alvarez.fing@gmail.com>
2026-01-20 17:21:57 -0300

    Ejercicios 02 - Refactorización del sistema de filtros para trabajar con el DOM

--- 02-ejercicio-javascript/filters.js
+++ 02-ejercicio-javascript/filters.js
@@ -1,5 +1,51 @@
+/* 
+
+Muy buena manera de resolverlo! Excelente trabajo!
+Vamos a cambiar algunas cosas por el cambio que hicimos en `fetch-data.js` para que funcione correctamente el estado de los botones de "Aplicar".
+
+*/
+
+const filterLocation = document.getElementById("filter-location");
+const filterLevel = document.getElementById("filter-experience-level");
+const filterTechnology = document.getElementById("filter-technology");
+const searchInput = document.getElementById("empleos-search-input");
+
+const filterJobs = () => {
+  // buscamos los jobs que tenemos en el DOM (todos, los mostrados y ocultos)
+  const jobsListingCard = document.querySelectorAll(".job-listing-card")
+
+  // valores que el usuario ingresa
+  const location = filterLocation.value.toLowerCase().trim();
+  const level = filterLevel.value.toLowerCase().trim();
+  const technology = filterTechnology.value.toLowerCase().trim();
+  const searchText = searchInput.value.toLowerCase().trim();
+
+  jobsListingCard.forEach((job) => {
+    // valores del elemento job
+    const jobLocation = job.dataset.location.toLowerCase()
+    const jobLevel = job.dataset.nivel.toLowerCase()
+    const jobTechnology = job.dataset.technology.toLowerCase().split(',')
+    const jobTitle = job.querySelector('h3').textContent.toLowerCase()
+
+    // comparamos valores
+    const matchLocation = location === "" || location === jobLocation
+    const matchLevel = level === "" || jobLevel === level
+    const matchTechnology = technology === "" || jobTechnology.includes(technology)
+    const matchSearch = searchText === "" || jobTitle.includes(searchText);
+
+    const isJobVisible = matchLocation && matchLevel && matchTechnology && matchSearch;
+    
+    job.style.display = isJobVisible ? "flex" : "none";
+  });
+};
+
+filterLocation.addEventListener("change", filterJobs);
+filterLevel.addEventListener("change", filterJobs);
+filterTechnology.addEventListener("change", filterJobs);
+searchInput.addEventListener("input", filterJobs);
+
 /* Aquí va la lógica para filtrar los resultados de búsqueda */
-import { jobs, renderJobs } from "./fetch-data.js";
+/* import { jobs, renderJobs } from "./fetch-data.js";
 
 const filterLocation = document.getElementById("filter-location");
 const filterLevel = document.getElementById("filter-experience-level");
@@ -30,4 +76,4 @@ const filterJobs = () => {
 filterLocation.addEventListener("change", filterJobs);
 filterLevel.addEventListener("change", filterJobs);
 filterTechnology.addEventListener("change", filterJobs);
-searchInput.addEventListener("input", filterJobs);
+searchInput.addEventListener("input", filterJobs); */
